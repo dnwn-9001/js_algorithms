434dee903a2d28934f82adf31b5fee694a86ebc8
wkdyi <cyi00@bbangyanetworks.com>
2023-02-21 18:44:28 +0900

    3-3.onlyNumber.js 다시

--- section3/3-3.onlyNumber.js
+++ section3/3-3.onlyNumber.js
@@ -1,13 +1,30 @@
 function solution(s) {
   let answer = 0;
-  const numArr = s.match(/[0-9]/g);
-  let num = 0;
-  for (let i = 0; i < numArr.length; i++) {
-    num += numArr[i];
-  }
-  answer = Number.parseInt(num);
+  // const numArr = s.match(/[0-9]/g);
+  // let num = 0;
+  // for (let i = 0; i < numArr.length; i++) {
+  //   num += numArr[i];
+  // }
+  // answer = Number.parseInt(num);
+
+  //다시
+  s = s.replace(/[^0-9]/g, "");
+  answer = parseInt(s);
+
+  //강의 풀이(parseInt 사용)
+  // for (let x of s) {
+  //   if (!isNaN(x)) answer += x;
+  // }
+
+  // return parseInt(answer);
+
+  //강의 풀이(parseInt 미사용)
+  // for (let x of s) {
+  //   if (!isNaN(x)) answer = answer * 10 + Number(x);
+  // }
+
   return answer;
 }
 
-const s = "g0e1n2T0s8eSo1f7t";
+const s = "g0en2T0s8eSoft";
 console.log(solution(s));
