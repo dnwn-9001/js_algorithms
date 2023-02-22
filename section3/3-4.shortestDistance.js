badf163446905930ad160cdd679ffd9d35ca9c1c
wkdyi <cyi00@bbangyanetworks.com>
2023-02-22 18:51:26 +0900

    3-4.가장 짧은 문자거리

---
+++ section3/3-4.shortestDistance.js
@@ -0,0 +1,30 @@
+function solution(s, t) {
+  let answer = [];
+  let p = 100;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== t) {
+      p++;
+      answer.push(p);
+    } else {
+      p = 0;
+      answer.push(p);
+    }
+  }
+
+  console.log(answer);
+  p = 100;
+  for (let k = s.length - 1; k >= 0; k--) {
+    if (s[k] !== t) {
+      p++;
+      answer[k] > p ? (answer[k] = p) : "";
+    } else {
+      p = 0;
+    }
+  }
+  return answer;
+}
+
+const s = "teachermode";
+const t = "e";
+console.log(solution(s, t));
