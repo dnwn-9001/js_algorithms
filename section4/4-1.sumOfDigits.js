608fde1feec0c9d4bf874e57215f7ea08aeb3de4
dnwn-9001 <j10511@hanmail.net>
2023-03-07 00:21:35 +0900

    자릿수의 합(내 풀이)

--- section4/4-1.sumOfDigits.js
+++ section4/4-1.sumOfDigits.js
@@ -1,9 +1,25 @@
 // 자릿수의 합
 function solution(arr) {
-  let answer = 0;
+  let answer,
+    sum = 0;
+  let max = Number.MIN_SAFE_INTEGER;
+
+  for (let x of arr) {
+    const str = x.toString();
+    sum = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      sum += Number.parseInt(str[i]);
+    }
+
+    if (sum > max) {
+      max = sum;
+      answer = x;
+    } else if (sum === max) answer = Math.max(answer, x);
+  }
 
   return answer;
 }
 
-const arr = [128, 460, 603, 40, 521, 137, 123];
+const arr = [128, 465, 663, 40, 521, 137, 123];
 console.log(solution(arr));
