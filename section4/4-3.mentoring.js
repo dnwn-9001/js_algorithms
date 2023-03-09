b42a56a8b293c2dcf5674885b4d46947d660a906
dnwn-9001 <j10511@hanmail.net>
2023-03-09 13:30:31 +0900

    멘토링 문제(1)

--- section4/4-3.mentoring.js
+++ section4/4-3.mentoring.js
@@ -1,5 +1,17 @@
 function solution(arr) {
   let answer = 0;
+  let numCases = new Set();
+  for (const x of arr) {
+    for (let i = 0; i < x.length; i++) {
+      for (let j = 1; j < x.length; j++) {
+        if (j > i) numCases.add([x[i], x[j]]);
+      }
+    }
+  }
+
+  for (let i = 0; i < numCases.length; i++) {}
+
+  console.log(numCases);
 
   return answer;
 }
@@ -9,4 +21,5 @@ const arr = [
   [4, 3, 2, 1],
   [3, 1, 4, 2],
 ];
+
 console.log(solution(arr));
